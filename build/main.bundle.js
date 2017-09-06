98e5f270d669aff363feb2a5f739cb6b343a1378
nartc <ctch5@mail.umsl.edu>
2017-09-06 18:41:44 -0500

    deploy 1.0.0

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -4378,7 +4378,7 @@ HttpService.host = "localhost";
 HttpService.port = 8080;
 HttpService.domain = "me-commerce.herokuapp.com";
 //public static url = "http://" + HttpService.host + ":" + HttpService.port;  
-HttpService.url = "http://" + HttpService_1.domain;
+HttpService.url = "https://" + HttpService_1.domain;
 HttpService = HttpService_1 = __decorate([
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(),
     __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_http__["Http"] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__angular_http__["Http"]) === "function" && _a || Object])
